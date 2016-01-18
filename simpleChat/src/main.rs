3860eae6bb038aeec2ae6caa41202aaed8b616b9
quxn6 <quxn6@naver.com>
2016-01-19 02:10:25 +0900

    마지막변경사항

--- simpleChat/src/main.rs
+++ simpleChat/src/main.rs
@@ -1,15 +1,11 @@
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::io::prelude::*;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::sync::{Arc, Mutex};
 
-type Sockets = HashMap<i32, TcpStream>;
+type Sockets = BTreeMap<i32, TcpStream>;
 type Messages = Vec<String>;
-
-// fn broadcasting(mut socket : TcpStream) {
-
-// }
 
 fn handle_client(mut socket: TcpStream, message_queue : Arc<Mutex<Messages>> ) {
     // ...
@@ -17,7 +13,14 @@ fn handle_client(mut socket: TcpStream, message_queue : Arc<Mutex<Messages>> ) {
     let mut buf : [u8; 128 ]= [0; 128];
     
     loop {
-	    socket.read(&mut buf).ok();
+	    match socket.read(&mut buf) {
+	    	Ok(len) => {println!("recieved {}", len);}
+	    	Err(e) => { 
+	    		println!("socket closed");
+	    		break; 
+	    	}
+	    }
+
 	    let message = String::from_utf8_lossy(&buf).into_owned();
 	    let mut mq = message_queue.lock().unwrap();
 	    mq.push(message);
@@ -28,6 +31,19 @@ fn broadcasting(users : Arc<Mutex<Sockets>>, message_queue : Arc<Mutex<Messages>
 	// users loop돌면서 messageQueue write
     // socket.write(&mut buf).ok();
     // socket.flush().ok();
+    loop {
+    	thread::sleep_ms(100);
+    	let mut mq = message_queue.lock().unwrap();
+    	let mut us = users.lock().unwrap();
+
+    	for (id, mut user_socket) in us.iter() {
+    		for msg in mq.iter() {
+    			user_socket.write(&mut msg.as_bytes()).ok();
+    		}
+    		user_socket.flush().ok();
+    	}
+    	mq.clear();
+    }
 }
 
 fn main() {
@@ -46,14 +62,17 @@ fn main() {
 	});
 
 	// accept connections and process them, spawning a new thread for each one
+	let mut user_count : i64 = 0;
 	for handle in listener.incoming() {
 	    match handle {
 	        Ok(socket) => {
+	        	user_count += 1;
 	        	let cloned_message_queue = message_queue.clone();
 	            thread::spawn(move|| {
 	                // connection succeeded
 	                handle_client(socket, cloned_message_queue)
 	            });
+	            println!("test spawn end");
 	        }
 	        Err(e) => { println!("{:?}", e); }
 	    }
